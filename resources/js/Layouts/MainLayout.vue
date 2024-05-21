fc34d405d8cc6cf4df5fe5bc8e0358b3a9164834
Denis Vykhodtsev <d.vyhodcev@softmg.ru>
2024-05-21 21:15:24 +0300

    Added navigation

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,29 +1,19 @@
 <script>
 import {Link} from "@inertiajs/vue3";
+import Navigation from "@/Components/Navigation.vue";
 
 export default {
     name: "MainLayout",
-    components: {Link}
+    components: {
+        Link,
+        Navigation
+    }
 }
 </script>
 
 <template>
     <div class="w-96 mx-auto">
-        <h1>My app</h1>
-        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-            <Link :href="route('dashboard')">
-                Dashboard
-            </Link>
-            <Link :href="route('project.index')">
-                Project
-            </Link>
-            <Link :href="route('project.import')">
-                Import
-            </Link>
-            <Link :href="route('task.index')">
-                Task
-            </Link>
-        </div>
+        <Navigation></Navigation>
         <slot/>
     </div>
 </template>
