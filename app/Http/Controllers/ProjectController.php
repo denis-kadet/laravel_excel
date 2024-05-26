bd5edc404f48e0ca10d74de90ba8203e7a8133bd
Denis Vykhodtsev <d.vyhodcev@softmg.ru>
2024-05-26 15:46:25 +0300

    Adding a file to Storage

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Project\ImportStoreRequest;
+use App\Models\File;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProjectController extends Controller
 {
@@ -20,6 +22,8 @@ class ProjectController extends Controller
     public function importStore(ImportStoreRequest $request)
     {
         $data = $request->validated();
-        dd($data);
+        $path = File::putAndCreate($data['file']);
+
+        dd($path);
     }
 }
