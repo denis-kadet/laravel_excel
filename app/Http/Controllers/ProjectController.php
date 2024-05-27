89653a71408f92040b5484338336964837b1c6f1
Denis Vykhodtsev <d.vyhodcev@softmg.ru>
2024-05-27 21:32:32 +0300

    Adding Laravel Excel and Creating a Job

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Project\ImportStoreRequest;
+use App\Jobs\ImportProjectExcelFileJob;
 use App\Models\File;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -24,6 +25,6 @@ class ProjectController extends Controller
         $data = $request->validated();
         $path = File::putAndCreate($data['file']);
 
-        dd($path);
+        ImportProjectExcelFileJob::dispatchNow($path);
     }
 }
